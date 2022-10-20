dc0f7c86c800ca923b4c39202e9a3241ccb5a0c3
liulq <zxc123456789963>
2022-10-20 23:02:58 +0800

    新增：变量

---
+++ javase/src/main/java/org/example/test/variable.java
@@ -0,0 +1,32 @@
+package org.example.test;
+
+import sun.applet.Main;
+
+/**
+ * @author liuliangqi
+ * @version V1.0
+ * @Package org.example.test
+ * @date 2022/10/19 23:24
+ * @Copyright 变量
+ */
+public class variable {
+    //程序的入口
+    public static void main(String[] args) {
+        //定义变量 : 变量数据类型  变量名称 =（赋值）
+        byte b=1;
+        System.out.println(b);
+        short s=2;
+        System.out.println(s);
+        int i=2028;
+        System.out.println(i);
+        char c ='女';
+        System.out.println(c);
+        boolean b2 =false;
+        System.out.println(b2);
+        long l =2029;
+        System.out.println(l);
+        //定义变量 3.14 默认情况下 是为double 类型 需要就在后面家f
+        float f=3.14f;
+        System.out.println(f);
+    }
+}
