b6502ffbec8e44d09387a90506359a3339219289
liulq <zxc123456789963>
2022-11-05 10:50:09 +0800

    新增：for循环案例

---
+++ javase/src/main/java/org/example/se/forTest.java
@@ -0,0 +1,30 @@
+package org.example.se;
+
+/**
+ * @author liuliangqi
+ * @version V1.0
+ * @Package org.example.se
+ * @date 2022/11/4 22:18
+ * @Copyright java的循环 do.. while   while... for..
+ */
+public class forTest {
+    public static void main(String[] args) {
+//     int x=1;
+//     while (x<5){
+//         System.out.println("hello world");
+//         x++;
+//     }
+
+//        int x=1;
+//        do {
+//            System.out.println("hello world");
+//            x++;
+//        }while (x<5);
+
+        for (int x=1;x<5;x++){
+            System.out.println("hello world");
+        }
+
+
+    }
+}
