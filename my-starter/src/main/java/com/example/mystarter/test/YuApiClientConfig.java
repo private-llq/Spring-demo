bc230556be2c61336059e50b80406ae642b5669e
liuliangqi <1649986432@qq.com>
2022-12-21 22:06:40 +0800

    新增：自定义starter

---
+++ my-starter/src/main/java/com/example/mystarter/test/YuApiClientConfig.java
@@ -0,0 +1,57 @@
+package com.example.mystarter.test;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author liuliangqi
+ * @version V1.0
+ * @Package com.example.mystarter.test
+ * @date 2022/12/21 21:47
+ * @description: 公司
+ */
+public class YuApiClientConfig {
+
+    /**
+     * appId
+     */
+    private String appId;
+
+    /**
+     * 秘钥
+     */
+    private String appSecret;
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getAppSecret() {
+        return appSecret;
+    }
+
+    public void setAppSecret(String appSecret) {
+        this.appSecret = appSecret;
+    }
+
+    public YuApiClientConfig() {
+    }
+
+    public YuApiClientConfig(String appId, String appSecret) {
+        this.appId = appId;
+        this.appSecret = appSecret;
+    }
+
+    @Override
+    public String toString() {
+        return "YuApiClientConfig{" +
+                "appId='" + appId + '\'' +
+                ", appSecret='" + appSecret + '\'' +
+                '}';
+    }
+}
