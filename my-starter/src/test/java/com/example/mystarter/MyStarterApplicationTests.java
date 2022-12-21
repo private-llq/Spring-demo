4fe0893af34f25bc6b23c09690d12762a1c205c3
liuliangqi <1649986432@qq.com>
2022-12-21 22:07:37 +0800

    新增：自定义starter-1

---
+++ my-starter/src/test/java/com/example/mystarter/MyStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mystarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
