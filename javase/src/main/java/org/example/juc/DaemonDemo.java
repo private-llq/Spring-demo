40c28f8a954d32ba6089148ef2bad0ea929c1361
liuliangqi <1649986432@qq,com>
2023-04-25 10:24:00 +0800

    新增：juc模块示例测试

---
+++ javase/src/main/java/org/example/juc/DaemonDemo.java
@@ -0,0 +1,31 @@
+package org.example.juc;
+
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * @author liulq
+ */
+public class DaemonDemo {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " 开始运行," + (Thread.currentThread().isDaemon() ? "守护线程" : "用户线程"));
+            while (true) {
+
+            }
+        }, "t1");
+        t1.setDaemon(true);//通过设置属性Daemon来设置当前线程是否为守护线程
+        t1.start();
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(Thread.currentThread().getName() + " 主线程结束");
+    }
+}
+
+
+//输出：t1 开始运行,守护线程
+//main 主线程结束--->在main主线程结束后，守护线程会伴随着JVM一同结束工作，即使还有循环没有结束
---
+++ javase/src/main/java/org/example/juc/JucTest.java
@@ -0,0 +1,18 @@
+package org.example.juc;
+
+/**
+ * @author liuliangqi
+ * @version V1.0
+ * @Package org.example.juc
+ * @date 2023/4/25 0:43
+ * @description: 公司
+ */
+public class JucTest {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() ->{
+        },"t1");
+        t1.start();
+
+    }
+
+}
