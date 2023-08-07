390e1dc60d7bf0d8b4cb3e00152f429850051ac5
liuliangqi <1649986432@qq,com>
2023-08-07 22:42:44 +0800

    新增：springcloud模块

---
+++ springcloud-demo/cloud-eureka-server7001/src/main/java/com/example/EurekaMain7001.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer  //表示当前是Eureka的服务注册中心
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
