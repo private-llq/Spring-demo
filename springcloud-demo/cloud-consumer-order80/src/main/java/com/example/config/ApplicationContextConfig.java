c8cf5aa75254c11f395b8361ff13e203de431523
liuliangqi <1649986432@qq,com>
2023-12-10 22:08:36 +0800

    修改：测试微服务

--- springcloud-demo/cloud-consumer-order80/src/main/java/com/example/config/ApplicationContextConfig.java
+++ springcloud-demo/cloud-consumer-order80/src/main/java/com/example/config/ApplicationContextConfig.java
@@ -1,4 +1,5 @@
 package com.example.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +10,27 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationContextConfig {
     @Bean
-    public RestTemplate getRestTemplate(){
+    public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        int size = 9;
+
+        // 打印表头
+        System.out.print("  |");
+        for (int i = 1; i <= size; i++) {
+            System.out.printf("%3d", i);
+        }
+        System.out.println("\n--+---------------------------");
+
+        // 打印表格
+        for (int i = 1; i <= size; i++) {
+            System.out.printf("%2d|", i);
+            for (int j = 1; j <= size; j++) {
+                System.out.printf("%3d", i * j);
+            }
+            System.out.println();
+        }
     }
 }
--- springcloud-demo/cloud-consumer-order80/src/main/java/com/example/controller/OrderController.java
+++ springcloud-demo/cloud-consumer-order80/src/main/java/com/example/controller/OrderController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import sun.text.resources.FormatData;
 
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
